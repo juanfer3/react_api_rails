71ebcdb3caea41888e3adb97ed576e8603831630
juanfer3 <jfvilladiego2@misena.edu.co>
2018-07-23 22:32:11 -0500

    Posts Authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-   # before_action :authenticate_user,  only: [:index]
+    before_action :authenticate_user,  only: [:index]
     before_action :set_post, only: [:show, :update, :destroy]
 
     def index
